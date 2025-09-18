fcedd4a79d0dc6df58aaab144c57da008b8fff77
ZhuHaoyang <1932232849@qq.com>
2025-09-18 15:01:39 +0800

    Output an Image

---
+++ InOneWeekend/2. Output an Image/2.1. The PPM Image Format/main.py
@@ -0,0 +1,28 @@
+def main():
+
+    # Image
+
+    image_width = 256
+    image_height = 256
+
+    # Render
+
+    print('P3')
+    print(f'{image_width} {image_height}')
+    print('255')
+
+    for j in range(image_height):
+        for i in range(image_width):
+            r = i / (image_width - 1)
+            g = j / (image_height - 1)
+            b = 0
+
+            ir = int(255.999 * r)
+            ig = int(255.999 * g)
+            ib = int(255.999 * b)
+
+            print(f'{ir} {ig} {ib}')
+
+
+if __name__ == '__main__':
+    main()
