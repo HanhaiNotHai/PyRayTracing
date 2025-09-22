69f41f123e2fda278dcde759b48eddbd7b09cdb1
ZhuHaoyang <1932232849@qq.com>
2025-09-22 15:00:40 +0800

    Simplifying the Ray-Sphere Intersection Code

---
+++ InOneWeekend/6. Surface Normals and Multiple Objects/6.2. Simplifying the Ray-Sphere Intersection Code/ray.py
@@ -0,0 +1,14 @@
+from vector import Point3, Vector3
+
+
+class Ray:
+
+    def __init__(self, origin: Point3, direction: Vector3):
+        self.origin = origin
+        self.direction = direction
+
+    def __repr__(self) -> str:
+        return f'Ray(origin = {self.origin}, direction = {self.direction})'
+
+    def at(self, t: float) -> Point3:
+        return self.origin + t * self.direction
