9cd85da056ca524d2ec3df4764d538c2d1d7d4d9
Hanhai <1932232849@qq.com>
2025-09-24 17:54:34 +0800

    Accelerate using threading (#34)

---
+++ InOneWeekend/Accelerate/threading/main.py
@@ -0,0 +1,65 @@
+import random
+
+from camera import Camera
+from color import Color
+from hittable_list import HittableList
+from material import Dielectric, Lambertian, Metal
+from sphere import Sphere
+from vector import Point3, Vector3
+
+
+def main():
+    world = HittableList()
+
+    ground_material = Lambertian(Color(0.5, 0.5, 0.5))
+    world.add(Sphere(Point3(0, -1000, 0), 1000, ground_material))
+
+    for a in range(-11, 11):
+        for b in range(-11, 11):
+            choose_mat = random.random()
+            center = Point3(a + 0.9 * random.random(), 0.2, b + 0.9 * random.random())
+
+            if (center - Point3(4, 0.2, 0)).length() > 0.9:
+                if choose_mat < 0.8:
+                    # diffuse
+                    albedo = Color.random() * Color.random()
+                    sphere_material = Lambertian(albedo)
+                    world.add(Sphere(center, 0.2, sphere_material))
+                elif choose_mat < 0.95:
+                    # metal
+                    albedo = Color.random(0.5, 1)
+                    fuzz = random.uniform(0.5, 1)
+                    sphere_material = Metal(albedo, fuzz)
+                    world.add(Sphere(center, 0.2, sphere_material))
+                else:
+                    # glass
+                    sphere_material = Dielectric(1.5)
+                    world.add(Sphere(center, 0.2, sphere_material))
+
+    material1 = Dielectric(1.5)
+    world.add(Sphere(Point3(0, 1, 0), 1, material1))
+
+    material2 = Lambertian(Color(0.4, 0.2, 0.1))
+    world.add(Sphere(Point3(-4, 1, 0), 1, material2))
+
+    material3 = Metal(Color(0.7, 0.6, 0.5), 0)
+    world.add(Sphere(Point3(4, 1, 0), 1, material3))
+
+    cam = Camera(
+        aspect_ratio=16 / 9,
+        image_width=320,
+        samples_per_pixel=10,
+        max_depth=5,
+        vfov=20,
+        lookfrom=Point3(13, 2, 3),
+        lookat=Point3(0, 0, 0),
+        vup=Vector3(0, 1, 0),
+        defocus_angle=0.6,
+        focus_dist=10,
+    )
+
+    cam.render_threading(world, num_threads=8)
+
+
+if __name__ == '__main__':
+    main()
